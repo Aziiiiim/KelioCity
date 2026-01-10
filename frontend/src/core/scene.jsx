449ff39ff15d409f390e977f58ea5379269da897
a23barho <azim.barhoumi@imt-atlantique.net>
2026-01-11 00:47:38 +0100

    serv + cicd

--- frontend/src/core/scene.jsx
+++ frontend/src/core/scene.jsx
@@ -42,7 +42,7 @@ export function createScene(){
     scene.groupCharacters = new THREE.Group();
     scene.add(scene.groupCharacters);
     const objectList = [];
-    fetch("http://localhost:8080/api/rooms")
+    fetch("/api/rooms")
        .then(res => res.json())
        .then(rooms => {
            for (let i=0; i < rooms.length; i++) {
@@ -75,7 +75,7 @@ export function createScene(){
                 roomList.push(roomElements);
                 scene.groupRooms.add(roomElements);
            }
-           fetch("http://localhost:8080/api/employees")
+           fetch("/api/employees")
                .then(res => res.json())
                .then(employees => {
                    let loadedChars = 0;
