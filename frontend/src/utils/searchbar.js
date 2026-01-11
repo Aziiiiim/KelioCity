ff7c0056ab191c01d89d735f77ac30999830b257
Arthur <arthur.loizeau@imt-atlantique.net>
2026-01-11 17:37:48 +0100

    add case when no filter button is selected

--- frontend/src/utils/searchbar.js
+++ frontend/src/utils/searchbar.js
@@ -33,7 +33,10 @@ function selectFilter(btn) {
 
 function showResults() {
     console.log("to be implemented");
-    const btnType = document.getElementsByClassName("filter-btn active")[0].dataset.type;
+    let btnType = "";
+    if (document.getElementsByClassName("filter-btn active")[0]) {
+        btnType = document.getElementsByClassName("filter-btn active")[0].dataset.type;
+    }
     const searchContent = document.getElementsByClassName("search-bar")[0].value;
     if (searchContent === "") {
         document.getElementsByClassName("search-dropdown")[0].classList.add("hidden");
@@ -118,6 +121,55 @@ function showResults() {
                         li_result.textContent = results[i]["deskName"];
                         dropdown.appendChild(li_result);
                     }
+                });
+        } else if (btnType === "") {
+            const dropdown = document.getElementsByClassName("search-dropdown")[0];
+            dropdown.innerHTML = "";
+            fetch("http://localhost:8080/api/employees/search/" + searchContent)
+                .then(res => res.json())
+                .then(results => {
+                    for (let i = 0; i < results.length; i++) {
+                        let li_result = document.createElement("li");
+                        li_result.classList.add("search-item");
+                        li_result.onclick = () => goToResult(li_result);
+                        li_result.textContent = results[i]["firstName"] + " " + results[i]["lastName"];
+                        dropdown.appendChild(li_result);
+                    }
+                    fetch("http://localhost:8080/api/rooms/search/" + searchContent)
+                        .then(res => res.json())
+                        .then(results => {
+                            for (let i = 0; i < results.length; i++) {
+                                let li_result = document.createElement("li");
+                                li_result.classList.add("search-item");
+                                li_result.onclick = () => goToResult(li_result);
+                                li_result.textContent = results[i]["roomName"];
+                                dropdown.appendChild(li_result);
+                            }
+                            fetch("http://localhost:8080/api/desks/search/" + searchContent)
+                                .then(res => res.json())
+                                .then(results => {
+                                    for (let i = 0; i < results.length; i++) {
+                                        let li_result = document.createElement("li");
+                                        li_result.classList.add("search-item");
+                                        li_result.onclick = () => goToResult(li_result);
+                                        li_result.textContent = results[i]["deskName"];
+                                        dropdown.appendChild(li_result);
+                                    }
+                                    if (dropdown.children.length === 0) {
+                                        let no_result = document.createElement("li");
+                                        no_result.classList.add("search-item");
+                                        no_result.textContent = "Aucun Résultat";
+                                        no_result.onclick = () => {
+                                            document.getElementsByClassName("search-bar")[0].value = "";
+                                            const btns = document.getElementsByClassName("filter-btn");
+                                            for (let i=0; i<btns.length; i++) {
+                                                btns[i].classList.remove("active");
+                                            }
+                                        };
+                                        dropdown.appendChild(no_result);
+                                    }
+                                });
+                        });
                 });
         }
     }
