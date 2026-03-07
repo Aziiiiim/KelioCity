8d532f58fbe455354cb3342732877c141c5884c4
Arthur <arthur.loizeau@imt-atlantique.net>
2026-03-07 19:04:38 +0100

    add Office2DesksB2

--- frontend/src/objects/Office2DesksB2.jsx
+++ frontend/src/objects/Office2DesksB2.jsx
@@ -1,80 +1,78 @@
 import * as THREE from 'three';
 import { makeInstance } from '../utils/asset.js'; 
     
-export function createOffice2Desks(initDoor = null) {
-    let x = -1;
+export function createOffice2DesksB2(initDoor = null) {
     
     const elements = new THREE.Group();
     elements.userData.kind = "room";
-    elements.userData.roomType = "Office2Desk"; 
+    elements.userData.roomType = "Office2DeskB2";
 
     // Wall and floor
-    const floorGeo = new THREE.PlaneGeometry(5, 6);
-    const floorMat = new THREE.MeshBasicMaterial( { color: 0xdedede } );
+    const floorGeo = new THREE.PlaneGeometry(6, 4.49);
+    const floorMat = new THREE.MeshBasicMaterial( { color: 0x2F4680 } );
     const floorMesh = new THREE.Mesh(floorGeo, floorMat);
     floorMesh.rotation.x = Math.PI * -.5;
-    floorMesh.position.set(3.5+x,0.01,3)
+    floorMesh.position.set(3,0.01,2.245)
     elements.add(floorMesh);
 
-    const wallGeo1 = new THREE.PlaneGeometry(5, 5);
-    const wallGeo2 = new THREE.PlaneGeometry(6, 5);
-    const wallMat = new THREE.MeshBasicMaterial( { color: 0x335c67, side: THREE.DoubleSide } );
+    const wallGeo1 = new THREE.PlaneGeometry(6, 4);
+    const wallGeo2 = new THREE.PlaneGeometry(4.49, 4);
+    const wallMat = new THREE.MeshBasicMaterial( { color: 0xD6D3D2, side: THREE.DoubleSide } );
 
     const wallMesh1 = new THREE.Mesh(wallGeo1, wallMat);
-    wallMesh1.position.set(3.5+x,2.5,0.01);
+    wallMesh1.position.set(3,2,0.01);
     elements.add(wallMesh1);
 
     const wallMesh2 = new THREE.Mesh(wallGeo2, wallMat);
     wallMesh2.rotation.y = Math.PI * -.5;
-    wallMesh2.position.set(5.99+x,2.5,3);
+    wallMesh2.position.set(5.99,2,2.245);
     elements.add(wallMesh2);
 
     const wallFrontLeft = new THREE.Mesh(
-        new THREE.PlaneGeometry(3.85, 5),
+        new THREE.PlaneGeometry(0.38, 4),
         wallMat
     );
     wallFrontLeft.rotation.y = Math.PI * 0.5;
-    wallFrontLeft.position.set(1 + x, 2.5, 1.925);
+    wallFrontLeft.position.set(0, 2, 0.2);
     elements.add(wallFrontLeft);
     const wallFrontTop = new THREE.Mesh(
-        new THREE.PlaneGeometry(2.1, 1.96),
+        new THREE.PlaneGeometry(2.3, 0.96),
         wallMat
     );
     wallFrontTop.rotation.y = Math.PI * 0.5;
-    wallFrontTop.position.set(1 + x, 4.02, 4.5);
+    wallFrontTop.position.set(0, 3.52, 1.5);
     elements.add(wallFrontTop);
     const wallFrontRight = new THREE.Mesh(
-        new THREE.PlaneGeometry(0.52, 5),
+        new THREE.PlaneGeometry(2.5, 4),
         wallMat
     );
     wallFrontRight.rotation.y = Math.PI * 0.5;
-    wallFrontRight.position.set(1 + x, 2.5, 5.74);
+    wallFrontRight.position.set(0, 2, 3.24);
     elements.add(wallFrontRight);
 
     const wallMesh4 = new THREE.Mesh(wallGeo1, wallMat);
     wallMesh4.rotation.y = Math.PI ;
-    wallMesh4.position.set(3.5+x,2.5,5.99);
+    wallMesh4.position.set(3,2,4.49);
     elements.add(wallMesh4);
 
     let doorPivot = null;
     let doorOpen = false;
     let doorProgress = 0;
+
     // Desk1
     makeInstance('/assets/models/decoratedDesk.glb')
         .then((desk) => {
-        desk.position.set(-5.43 + x, 0, 2.5);
-        desk.scale.set(1.1, 1.1, 1.1);
-        desk.rotation.y = -Math.PI * 0.5;
+        desk.position.set(4.37, 0, 8.8);
+        desk.scale.set(-1.1, 1.1, 1.1);
         elements.add(desk);
         })
         .catch(console.error);
 
     // Chair1
-    makeInstance('/assets/models/chair.glb')
+    makeInstance('/assets/models/OfficeChair.glb')
         .then((chair) => {
-        chair.position.set(-4.7 + x, 0, -9.5);
-        chair.scale.set(0.035, 0.035, 0.035);
-        chair.rotation.y = -Math.PI * 0.5;
+        chair.position.set(4.7, 0, 1);
+        chair.scale.set(1.3, 1.3, 1.3);
         elements.add(chair);
         })
         .catch(console.error);
@@ -82,40 +80,88 @@ export function createOffice2Desks(initDoor = null) {
     // Desk2
     makeInstance('/assets/models/decoratedDesk.glb')
         .then((desk) => {
-        desk.position.set(12.43 + x, 0, 3.6);
-        desk.scale.set(1.1, 1.1, 1.1);
-        desk.rotation.y = Math.PI * 0.5;
+        desk.position.set(1.61, 0, -4.4);
+        desk.scale.set(-1.1, 1.1, 1.1);
+        desk.rotation.y = Math.PI;
         elements.add(desk);
         })
         .catch(console.error);
 
     // Chair2
-    makeInstance('/assets/models/chair.glb')
+    makeInstance('/assets/models/OfficeChair.glb')
         .then((chair) => {
-        chair.position.set(11.7 + x, 0, 15.7);
-        chair.scale.set(0.035, 0.035, 0.035);
-        chair.rotation.y = Math.PI * 0.5;
+        chair.position.set(1.2, 0, 3.4);
+        chair.scale.set(1.3, 1.3, 1.3);
+        chair.rotation.y = Math.PI;
         elements.add(chair);
         })
         .catch(console.error);
 
-    // Message Board
-    makeInstance('/assets/models/messageBoard.glb')
+    // White Board
+    makeInstance('/assets/models/Whiteboard.glb')
         .then((board) => {
-        board.position.set(3 + x, 1, 0.1);
-        board.scale.set(0.02, 0.02, 0.02);
-        board.rotation.y = Math.PI * -0.5;
+        board.position.set(1.5, 1, 0.01);
+        board.scale.set(0.25, 0.3, 0.5);
         elements.add(board);
         })
         .catch(console.error);
 
-    // Shelf
-    makeInstance('/assets/models/containerShelf.glb')
-        .then((shelf) => {
-        shelf.position.set(5.6 + x, 1.14, 0.8);
-        shelf.scale.set(0.03, 0.03, 0.03);
-        shelf.rotation.y = Math.PI * 0.5;
-        elements.add(shelf);
+    // Fan
+    makeInstance('/assets/models/Fan.glb')
+        .then((fan) => {
+        fan.position.set(5.6, 0, 4);
+        fan.scale.set(1.5, 2, 1.5);
+        fan.rotation.y = -Math.PI/4;
+        elements.add(fan);
+        })
+        .catch(console.error);
+
+    // Cabin
+    makeInstance('/assets/models/Cabin.glb')
+        .then((cabin) => {
+        cabin.position.set(2.8, 0.8, 4.05);
+        cabin.scale.set(2, 2, 2);
+        cabin.rotation.y = Math.PI;
+        elements.add(cabin);
+        })
+        .catch(console.error);
+
+    // Bin
+    makeInstance('/assets/models/Bin.glb')
+        .then((bin) => {
+        bin.position.set(4.1, 0.39, 4);
+        bin.scale.set(0.5, 0.5, 1);
+        elements.add(bin);
+        })
+        .catch(console.error);
+
+    // Wooden Chair 1
+    makeInstance('/assets/models/WoodenChair.glb')
+        .then((chair) => {
+        chair.position.set(4.9, 0, 3.8);
+        chair.scale.set(0.15, 0.15, 0.15);
+        chair.rotation.y = Math.PI/2;
+        elements.add(chair);
+        })
+        .catch(console.error);
+
+    // Wooden Chair 2
+    makeInstance('/assets/models/WoodenChair.glb')
+        .then((chair) => {
+        chair.position.set(3.2, 0, 0.5);
+        chair.scale.set(0.15, 0.15, 0.15);
+        chair.rotation.y = -Math.PI/2;
+        elements.add(chair);
+        })
+        .catch(console.error);
+
+    // Window
+    makeInstance('/assets/models/Window.glb')
+        .then((window) => {
+        window.position.set(6, 1.3, 2.25);
+        window.scale.set(4.3, 1.8, 1.8);
+        window.rotation.y = Math.PI/2;
+        elements.add(window);
         })
         .catch(console.error);
 
@@ -129,7 +175,7 @@ export function createOffice2Desks(initDoor = null) {
         doorPivot.userData.action = "toggleDoor";
         doorPivot.userData.toggleDoor = toggleDoor;
 
-        doorPivot.position.set(1 + x, 1.5, 5.48);
+        doorPivot.position.set(0, 1.5, 2);
         doorObj.position.set(-0.88, 0, 0);
 
         doorPivot.add(doorObj);
